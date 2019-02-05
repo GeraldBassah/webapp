9c41b50c1e083d21287bf1c8c4926b74573013fc
Gerald <bassahgerald@gmail.com>
2019-02-05 14:23:07 +0000

    track

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, CardText, IconButton, Footer, FooterLinkList, FooterSection, Table, TableHeader } from 'react-mdl';
 import { Grid, Row, Col } from "react-bootstrap";
+import { Card } from "components/Card/Card.jsx";
+import { StatsCard } from "components/StatsCard/StatsCard.jsx";
+import { Tasks } from "components/Tasks/Tasks.jsx";
 class DashBoard extends Component {
   constructor(props) {
     super(props);
