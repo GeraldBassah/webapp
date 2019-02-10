df2126c2573b691b23edf533d425afc3c2c154b9
Gerald <bassahgerald@gmail.com>
2019-02-10 01:22:51 +0000

    post

--- src/Components/SecdashboardView.jsx
+++ src/Components/SecdashboardView.jsx
@@ -3,8 +3,9 @@ import "./App.css";
 import "./App.js";
 import { Link } from 'react-router-dom'
 import Routes from "./Components/Routes";
+
 //import DashBoard from "./Components/DashBoard.jsx";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList } from "react-mdl";
 
 
 class SecDashboardView extends Component {
@@ -20,16 +21,23 @@ class SecDashboardView extends Component {
                     </Header>
                     <Drawer className="drawer" title="WELCOME" >
                         <Navigation>
-                            <Link to="sec/dashboard/main">DashBoard</Link>
-                            <Link to="sec/dashboard/activeCollectors">Active Collectors</Link>
-                            <Link to="sec/dashboard/help">Help</Link>
-                            <Link to="sec/dashboard/help">Help</Link>
+                            <Link to="sec/dashboard/main"><i class="fas fa-signal"></i>DashBoard</Link>
+                            <Link to="sec/dashboard/activeCollectors"><i class="fas fa-walking"></i>Active Collectors</Link>
+                            <Link to="sec/dashboard/help"><i class="fas fa-question-circle"></i>Help</Link>
                         </Navigation>
                     </Drawer>
                     <Content>
                         <div className="page-content" />
                         <Routes />
                     </Content>
+                    <Footer className="footer" size="mini" style={{ height: '5px' }}>
+                        <FooterSection type="left" logo="MMDA">
+                            <FooterLinkList>
+                                <a href="/">Help</a>
+                                <a href="/">Privacy & Terms</a>
+                            </FooterLinkList>
+                        </FooterSection>
+                    </Footer>
                 </Layout>
             </div>
         );
